b04d5a15ae2eae334e40ff5571c8ebe54774c951
TomaszSowa <tsowa974@gmail.com>
2018-04-19 14:37:45 +0200

    update main

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,5 +3,5 @@ package main // define package
 import "fmt" // import necessary package to print to console
 
 func main() { // define main function
-    fmt.Println("hello world") // print hello world
+    fmt.Println("Hello DreamLab Academy") // print hello world
 }
